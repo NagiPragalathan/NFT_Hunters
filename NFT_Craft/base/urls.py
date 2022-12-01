c4a034e7008f1217247eb5d6db51eba397fef040
kk2069037@gmail.com <NagiPragalathan>
2022-12-01 09:22:07 +0500

    created turtle connection

--- NFT_Craft/base/urls.py
+++ NFT_Craft/base/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lobby),
+    path('',views.home),
+    path('home',views.home),
+    path('browse',views.browse),
+    path('profile',views.profile),
+    path('details',views.details),
+    path('streams',views.streams),
+    path('lobby', views.lobby),
     path('room/', views.room),
     path('get_token/', views.getToken),
+    path('open_Nft_craft',views.open_Nft_craft),
 
     path('create_member/', views.createMember),
     path('get_member/', views.getMember),
--- NFT_Craft/base/views.py
+++ NFT_Craft/base/views.py
@@ -7,6 +7,66 @@ from .models import RoomMember
 import json
 from django.views.decorators.csrf import csrf_exempt
 
+from turtle import *
+
+
+def home(request):
+    return render(request,'game_template/index.html')
+def browse(request):
+    return render(request,'game_template/browse.html')
+def details(request):
+    return render(request,'game_template/details.html')
+def streams(request):
+    return render(request,'game_template/streams.html')
+def profile(request):
+    return render(request,'game_template/profile.html')
+
+# workings
+def open_Nft_craft(request):
+    hideturtle()
+    pensize(10)
+    fillcolor('green')
+    speed(8)
+
+
+
+    right(90)
+    begin_fill()
+    forward(50)
+    for n in [
+    (30,200), (90, 200), (30,50)
+    ]:
+        circle(n[0], 180)
+        forward(n[1])
+        right(90)
+        forward(60)
+        end_fill()
+
+        penup()
+        goto(0, 100)
+        pendown()
+
+        fillcolor('white')
+        begin_fill()
+    for n in range(2):
+        forward(49)
+        circle(40, 180)
+        end_fill()
+
+        penup()
+        goto(-122, 150)
+        pendown()
+
+        fillcolor('green')
+        begin_fill()
+        left(180)
+    for i in [20, 150]:
+        forward(i)
+        circle(10, 90)
+        forward(20)
+        end_fill()
+    done()
+
 
 
 # Create your views here.
@@ -67,4 +127,3 @@ def deleteMember(request):
     return JsonResponse('Member deleted', safe=False)
 
 
-    
